bb5f76b9de196b70209ecb2c54dc049194f8f11d
Felipe Marques <lipemarques8@gmail.com>
2016-11-27 03:10:50 -0300

    Update app.js
    Ajustes

--- app.js
+++ app.js
@@ -1,39 +1,36 @@
 'use strict';
 
 const objs = require('./contas.json');
-
 var debitos = [];
 var creditos = [];
 
-function mapToChildrens (objs) {
+function mapToChildrens(objs) {
   objs = objs.sort(sortByDotsCount);
 
   const topCount = getCodeDotsCount(objs[0]);
   const elders = objs.filter(filterByDotsCount(topCount));
 
-  return elders.map(mapElders);
+  return elders.map((elder) => {
+    const descendants = getDescendants(objs, elder.code);
+    elder.children = descendants.length > 0 ? mapToChildrens(descendants) : [];
+    return elder;
+  });
 }
 
-function mapElders (elder) {
-  const descendants = getDescendants(objs, elder.code);
-  elder.children = descendants.length > 0 ? mapToChildrens(descendants) : [];
-  return elder;
-}
-
-function getDescendants (objs, parentCode) {
+function getDescendants(objs, parentCode) {
   return objs.filter(item => item.code.startsWith(parentCode) && item.code !== parentCode);
 }
 
-function filterByDotsCount (equal) {
+function filterByDotsCount(equal) {
   return item => getCodeDotsCount(item) === equal;
 }
 
-function sortByDotsCount (a, b) {
+function sortByDotsCount(a, b) {
   return getCodeDotsCount(a) - getCodeDotsCount(b);
 }
 
-function getCodeDotsCount (item) {
+function getCodeDotsCount(item) {
   return item.code.split('.').length;
 }
 
-console.log(JSON.stringify(mapToChildrens(objs),"", "  "));
+console.log(JSON.stringify(mapToChildrens(objs), "", "  "));
